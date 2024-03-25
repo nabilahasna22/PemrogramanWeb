9672fa46cc8560793fa2cbf18f04deff5c60fa74
nabilahasna22 <nabil4hasn4@gmail.com>
2024-03-26 06:49:03 +0700

    Jobsheet 6

--- Pertemuann6/fungsiRekursif.php
+++
@@ -1,7 +0,0 @@
-<?php
-function tampilkanHaloDunia(){
-    echo "Halo Duniaaa <br>";
-    tampilkanHaloDunia();
-}
-tampilkanHaloDunia();
-?>
--- Pertemuann6/variabelServer.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo json_encode($_SERVER);
-// Mengonversi seluruh isi dari variabel $_SERVER menjadi format JSON
-?>
